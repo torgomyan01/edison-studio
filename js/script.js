d9c3067c679a86c70c552a8c94de6eec5deab956
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2023-06-30 23:07:12 +0400

    en project edison studio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const { none, show, active, hidden, opacity25, disabled } = {
+    none: "d-none",
+    show: 'show',
+    active: 'active',
+    hidden: 'overflow-hidden',
+    opacity25: 'opacity-25',
+    disabled: 'disabled'
+}
+
